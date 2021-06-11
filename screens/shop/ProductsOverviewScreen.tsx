a79690677038798a54ed1389a3a868c49e7f9bc6
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-11 13:44:16 -0600

    inital state and data set up

---
+++ screens/shop/ProductsOverviewScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FlatList, View } from 'react-native'
+
+
+const ProductsOverviewScreen = (props) => {
+    return (
+        <FlatList />
+    )
+}
+
+export default ProductsOverviewScreen
