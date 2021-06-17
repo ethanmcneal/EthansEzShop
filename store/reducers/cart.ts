f20ff57bfbad1d4ae9f8444e628bda734db4971b
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-17 12:40:21 -0600

    code optimization in cart reducer

--- store/reducers/cart.ts
+++ store/reducers/cart.ts
@@ -3,7 +3,7 @@ import { ADD_TO_CART } from "../actions/cart";
 
 const initialState = {
     items: {},
-    sum: 0
+    totalAmount: 0
 };
 
 export default (state :any = initialState, action :any) => {
@@ -13,24 +13,23 @@ export default (state :any = initialState, action :any) => {
             const prodPrice = addedProduct.price;
             const prodTitle = addedProduct.title;
 
+            let nextItem;
             if (state.items[addedProduct.id]){
-                const updatedCartItem = new CartItem(
+                nextItem = new CartItem(
                     state.items[addedProduct.id].quantity + 1,
                     prodPrice,
                     prodTitle,
                     state.items[addedProduct.id].sum + prodPrice
                 );
-                return {
-                    ...state, 
-                    items: {...state.items,[addedProduct.id]: updatedCartItem }
-                }
                 
             } else {
-                const newCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice)
-                return {
-                    ...state,
-                    items: {...state.items, [addedProduct.id]: newCartItem}
-                }
+                nextItem = new CartItem(1, prodPrice, prodTitle, prodPrice)
+                
+            }
+            return {
+                ...state,
+                items: {...state.items, [addedProduct.id]: nextItem},
+                totalAmount: state.totalAmount + prodPrice
             }
             break;
     
