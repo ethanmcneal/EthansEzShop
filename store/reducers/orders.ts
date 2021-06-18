a756f414cbd82a6258b05e2fa696299ceda87b87
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-18 12:19:56 -0600

    add order reducer logic

--- store/reducers/orders.ts
+++ store/reducers/orders.ts
@@ -1,16 +1,27 @@
+import Order from "../../models/order";
 import { ADD_ORDER } from "../actions/orders";
 
 const initialState = {
-    orders: []
-}
+	orders: [],
+};
 
-export default (state = initialState, action :any) => {
-    switch (action.type) {
-        case ADD_ORDER:
-            
-            break;
-    
-        default:
-            break;
-    }
-}
+export default (state = initialState, action: any) => {
+	switch (action.type) {
+		case ADD_ORDER:
+			const newOrder = new Order(
+				new Date().toString(),
+				action.orderData.items,
+				action.orderData.amount,
+				new Date().toString(),
+			);
+
+            return {
+                ...state,
+                orders: [...state.orders, newOrder]
+            }
+			break;
+
+		default:
+			break;
+	}
+};
