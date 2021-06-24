44b21be2647b726183ae22ca366f2360305a2bd9
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-24 15:40:59 -0600

    form hashed out

--- screens/user/EditProductScreen.tsx
+++ screens/user/EditProductScreen.tsx
@@ -1,19 +1,63 @@
 import React from 'react'
 
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import CustomHeaderButton from '../../components/UI/CustomHeaderButton'
 
 const EditProductScreen = (props :any) => {
 
     return(
-    <View>
-        <Text>Edit Products</Text>
-    </View>)
+    <ScrollView>
+        <View style={styles.form}>
+        <View>
+            <Text style={styles.label}>Title</Text>
+            <TextInput style={styles.input}/>
+        </View>
+        <View>
+            <Text style={styles.label}>Image URL</Text>
+            <TextInput style={styles.input}/>
+        </View>
+        <View>
+            <Text style={styles.label}>Price</Text>
+            <TextInput style={styles.input}/>
+        </View>
+        <View>
+            <Text style={styles.label}>Description</Text>
+            <TextInput style={styles.input}/>
+        </View>
+        </View>
+    </ScrollView>
+    )
 }
 
+EditProductScreen.navigationOptions = (navData :any) => {
+    return {
+        headerTitle: navData.navigation.getParam('productId') ? 'Edit Product' : 'Add Product',
+        headerRight: () => { 
+            return (<HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+                <Item title='Save' iconName={'ios-checkmark'} onPress={() => {
+                    ;
+                }}/>
+            </HeaderButtons>
+            )},
+    }
+}
 
 const styles = StyleSheet.create({
     screen: {
-        
-    }
+        width: '100%'
+    },
+    form: {
+        margin: 20,
+    },
+    label: {
+        marginVertical: 8,
+    },
+    input: {
+        paddingHorizontal: 2,
+        paddingVertical: 5,
+        borderColor: '#ccc',
+        borderBottomWidth: 1,
+    },
 })
 export default EditProductScreen
