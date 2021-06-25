0135204a099d4955e1ff1a66d5f204d328ed0fc9
ethan.mcneal <ethan.mcneal@gmail.com>
2021-06-24 18:17:16 -0600

    delete confirmation

--- screens/user/UserProducts.tsx
+++ screens/user/UserProducts.tsx
@@ -1,49 +1,77 @@
-import React from 'react';
-import { FlatList } from 'react-native';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { useSelector, useDispatch } from 'react-redux';
-import ProductCard from '../../components/ProductCard';
-import CustomHeaderButton from '../../components/UI/CustomHeaderButton';
-import * as productsActions from '../../store/actions/products'
+import React from "react";
+import { Alert, FlatList } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { useSelector, useDispatch } from "react-redux";
+import ProductCard from "../../components/ProductCard";
+import CustomHeaderButton from "../../components/UI/CustomHeaderButton";
+import * as productsActions from "../../store/actions/products";
 
+const UserProducts = (props: any) => {
+	const handleDelete = (id: string, title :string) => {
+		Alert.alert("Delete", `Delete ${title}?`, [
+			{ text: "No", style: "default" },
+			{
+				text: "Delete",
+				style: "destructive",
+				onPress: () => {
+					dispatch(productsActions.deleteProduct(id));
+				},
+			},
+		]);
+	};
 
-const UserProducts = (props :any) => {
+	const dispatch = useDispatch();
+	const userProds = useSelector((state: any) => state.products.userProducts);
+	return (
+		<FlatList
+			data={userProds}
+			keyExtractor={(item) => item.id}
+			renderItem={(itemData) => (
+				<ProductCard
+					itemData={itemData.item}
+					handleEditButton={() => {
+						props.navigation.navigate("EditProduct", {
+							productId: itemData.item.id,
+						});
+					}}
+					handleDeleteButton={() => {
+						handleDelete(itemData.item.id, itemData.item.title);
+					}}
+				/>
+			)}
+		/>
+	);
+};
 
-    const dispatch = useDispatch()
-    const userProds = useSelector((state :any) => state.products.userProducts)
-    return <FlatList 
-    data={userProds} 
-    keyExtractor={item => item.id} 
-    renderItem={(itemData) => <ProductCard 
-                                itemData={itemData.item}
-                                handleEditButton={() => {
-                                    props.navigation.navigate('EditProduct', 
-                                                             {productId: itemData.item.id})
-                                }}
-                                handleDeleteButton={() => {
-                                    dispatch(productsActions.deleteProduct(itemData.item.id))
-                                }}
-                                />}/>
-}
-
-
-UserProducts.navigationOptions = (navData :any) => {
-	return{
-	headerTitle: "Your Items",
-	headerLeft: () => { 
-		return (<HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
-			<Item title='Menu' iconName={'ios-menu'} onPress={() => {
-				navData.navigation.toggleDrawer();
-			}}/>
-		</HeaderButtons>
-		)},
-    headerRight: () => { 
-    return (<HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
-        <Item title='Add' iconName={'ios-create'} onPress={() => {
-            navData.navigation.navigate('EditProduct');
-        }}/>
-    </HeaderButtons>
-    )},
-    }
-}
-export default UserProducts
+UserProducts.navigationOptions = (navData: any) => {
+	return {
+		headerTitle: "Your Items",
+		headerLeft: () => {
+			return (
+				<HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+					<Item
+						title="Menu"
+						iconName={"ios-menu"}
+						onPress={() => {
+							navData.navigation.toggleDrawer();
+						}}
+					/>
+				</HeaderButtons>
+			);
+		},
+		headerRight: () => {
+			return (
+				<HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+					<Item
+						title="Add"
+						iconName={"ios-create"}
+						onPress={() => {
+							navData.navigation.navigate("EditProduct");
+						}}
+					/>
+				</HeaderButtons>
+			);
+		},
+	};
+};
+export default UserProducts;
